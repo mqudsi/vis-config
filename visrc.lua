b9bee159d0d70f0be72dbca3e80215cea61f41b4
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2017-11-03 12:42:13 -0500

    Added <C-s> binding to save

--- visrc.lua
+++ visrc.lua
@@ -5,6 +5,10 @@ vis.events.subscribe(vis.events.INIT, function()
 	--global configuration--
 	vis:command('set change-256colors off') --see vis#613
 	vis:command('set theme base16-tomorrow-night')
+
+	--keyboard shortcuts--
+	vis:command('map insert <C-s> <Escape>:w<Enter>')
+	vis:command('map normal <C-s> :w<Enter>')
 end)
 
 vis.events.subscribe(vis.events.WIN_OPEN, function(win)
