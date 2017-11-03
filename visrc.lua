660e3d597e6d8db9bd536b5c746f57c5f6458103
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2017-11-03 12:42:03 -0500

    Move theme configuration to global section

--- visrc.lua
+++ visrc.lua
@@ -4,12 +4,12 @@ require('plugins/filetype')
 vis.events.subscribe(vis.events.INIT, function()
 	--global configuration--
 	vis:command('set change-256colors off') --see vis#613
+	vis:command('set theme base16-tomorrow-night')
 end)
 
 vis.events.subscribe(vis.events.WIN_OPEN, function(win)
 	--per-window configuration--
 	vis:command('set number')
 	vis:command('set autoindent on')
-	vis:command('set theme base16-tomorrow-night')
 end)
 
